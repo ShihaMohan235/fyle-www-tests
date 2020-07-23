c35ebc6215cd78665dd88c0fd1672e9373a2b5df
Shiha Mohan <shiha.mohan@fyle.in>
2020-07-23 17:05:44 +0530

    added desktoptest cases for h1

---
+++ expense-report-template/desktop/template.py
@@ -0,0 +1,47 @@
+import os
+import time
+from simplebrowser import SimpleBrowser
+import logging
+import pytest
+
+logger = logging.getLogger(__name__)
+
+# TODO baseurl has to be changed
+@pytest.fixture
+def browser(module_browser, base_url):
+    module_browser.get(base_url+'/templates/expense-reports')
+    time.sleep(3)
+    return module_browser
+
+# def element_present(browser, element, message):
+#     e = browser.find_many(xpath=element)
+#     assert len(e)!=0, message
+
+# def test_h1_present(browser):
+#     element_present(browser, '//h1', "There should be atleast one h1 in page")
+
+# def test_h2_present(browser):
+#     element_present(browser, '//h2', "There should be atleast one h2 in page")
+
+def test_h1_text(browser):
+    e = browser.find(xpath='//h1')
+    assert e.text == "Free Expense Report Templates", "h1 text is not matching"
+
+def test_element_present(browser):
+    h1 = browser.find_many(xpath='//h1')
+    assert len(h1)!=0, "There should be atleast one h1 in page"
+    h2 = browser.find_many(xpath='//h2')
+    assert len(h2)!=0, "There should be atleast one h2 in page"
+
+def test_h1_count(browser):
+    e = browser.find_many(xpath='//h1')
+    assert len(e)==1, "Expected h1 count is one"
+
+def test_element_fontsize(browser):
+    h1 = browser.find(xpath='//h1').value_of_css_property('font-size')
+    assert h1=='50px', "expected font size of h1 is 50px"
+    h2 = browser.find(xpath='//h2').value_of_css_property('font-size')
+    assert h2=='30px', "expected font size of h2 is 30px"
+
+
+
---
+++ expense-report-template/mobile/template.py
@@ -0,0 +1,30 @@
+import os
+import time
+from simplebrowser import SimpleBrowser
+import logging
+import pytest
+
+logger = logging.getLogger(__name__)
+
+# TODO baseurl has to be changed
+@pytest.fixture
+def browser(module_browser, base_url):
+    module_browser.get(base_url+'/templates/expense-reports')
+    time.sleep(3)
+    return module_browser
+
+def test_element_present(browser):
+    h1 = browser.find_many(xpath='//h1')
+    assert len(h1)!=0, "There should be atleast one h1 in page"
+    h2 = browser.find_many(xpath='//h2')
+    assert len(h2)!=0, "There should be atleast one h2 in page"
+
+def test_h1_count(browser):
+    e = browser.find_many(xpath='//h1')
+    assert len(e)==1, "Expected h1 count is one"
+
+# def test_h1_fontsize(browser):
+#     h1 = browser.find(xpath='//h1').value_of_css_property('font-size')
+#     assert h1=='30px', "expected font size of h1 is 30px"
+
+
